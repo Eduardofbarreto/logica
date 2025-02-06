05a1bd744fdfcf175fb872cd375d5dbf3850da5e
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-02-06 11:07:09 -0300

    media

--- logica.cpp
+++ logica.cpp
@@ -2,15 +2,22 @@
 
 int main(){
     
+int num1, num2, num3;
+float media;
 
+std::cout<<"Digite o primeiro numero: "<<std::endl;
+std::cin>>num1;
 
-bool resposta = (8 + 9 != 17);
+std::cout<<"Digite o segundo numero: "<<std::endl;
+std::cin>>num2;
 
-bool resposta1 = (3 > 4);
+std::cout<<"Digite o terceiro numero: "<<std::endl;
+std::cin>>num3;
 
+media = (num1 + num2 + num3)/3;
 
-std::cout<<resposta<<std::endl;
-std::cout<<resposta1<<std::endl;
+std::cout<<"O resultado da média é: "<<media<<std::endl;
+
 
 return 0;
 
