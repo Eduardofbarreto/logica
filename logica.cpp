a34ae8960bc8eaf35c2ba67e12a2a78917e7c339
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-02-06 16:11:04 -0300

    insert

--- logica.cpp
+++ logica.cpp
@@ -8,7 +8,7 @@ std::string palavra;
 std::cout<<"Digite uma palavra: "<<std::endl;
 std::cin>>palavra;
 
-std::cout<<palavra.append(palavra)<<std::endl;
+std::cout<<palavra.insert(2,palavra)<<std::endl;
 
 return 0;
 
