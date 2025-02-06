05a851bb5c4b84ed06d6a4c0e5a89832b8a59ab6
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-02-06 18:08:11 -0300

    tamanho de texto concatenado

--- logica.cpp
+++ logica.cpp
@@ -3,12 +3,17 @@
 
 int main(){
 
-std::string nome;
+std::string nome, sobrenome;
 
 std::cout<<"Qual seu nome: "<<std::endl;
 std::cin>>nome;
 
-std::cout<<nome.length()<<std::endl;
+std::cout<<"Qual seu sobrenome: "<<std::endl;
+std::cin>>sobrenome;
+
+std::cout<<"Your name is " + nome + " and your last name is " + sobrenome)
+
+std::cout<<nome.length()<<sobrenome.length()<<std::endl;
 
 return 0;
 
