8746c58fef785054500a67e4694c0011f5bc0b57
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-02-06 12:50:18 -0300

    area de um triangulo

--- logica.cpp
+++ logica.cpp
@@ -2,33 +2,19 @@
 
 int main(){
     
-int nota1, nota2, nota3, soma;
-float media, n1, n2, n3;
+int base, altura;
+float area;
 
-std::cout<<"Digite a primeira nota: "<<std::endl;
-std::cin>>nota1;
+std::cout<<"Digite a altura: "<<std::endl;
+std::cin>>altura;
 
-n1 = nota1 * 0.20;
+std::cout<<"Digite a base: "<<std::endl;
+std::cin>>base;
 
-std::cout<<"Digite a segunda nota: "<<std::endl;
-std::cin>>nota2;
+area = (base * altura)/2;
 
-n2 = nota2 * 0.35;
+std::cout<<"A area do triangulo e de: "<<area<<std::endl;
 
-std::cout<<"Digite a terceira nota: "<<std::endl;
-std::cin>>nota3;
-
-n3 = nota3 * 0.45;
-
-media = (n1 + n2 + n3)/3;
-soma = n1 + n2 + n3;
-
-std::cout<<"\n"<<std::endl;
-std::cout<<"Parcial 1: "<<n1<<std::endl;
-std::cout<<"Parcial 2: "<<n2<<std::endl;
-std::cout<<"Parcial 3: "<<n3<<std::endl;
-std::cout<<"A soma das parciais e de: "<<soma<<std::endl;
-std::cout<<"A media das parciais e de: "<<media<<std::endl;
 
 return 0;
 
