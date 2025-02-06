8fe32b17ce81c53c7f9832b0c01c1aa102d0bc18
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-02-06 11:15:56 -0300

    ano bissexto

--- logica.cpp
+++ logica.cpp
@@ -2,15 +2,16 @@
 
 int main(){
     
-int num;
+int year;
 bool resposta;
 
-std::cout<<"Digite um numero: "<<std::endl;
-std::cin>>num;
+std::cout<<"Digite um ano: "<<std::endl;
+std::cin>>year;
 
-resposta = (num %2 == 0);
+resposta = (year % 4 == 0 or year % 100 == 0 && year % 400 == 0);
 
 std::cout<<resposta<<std::endl;
+
 
 
 return 0;
