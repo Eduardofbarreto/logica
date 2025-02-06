863b0e23fb9827a23179be9a361975b454ca174f
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-02-06 10:51:17 -0300

    usando bool

---
+++ logica.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+int main(){
+    
+
+
+bool resposta = (8 + 9 != 17);
+
+bool resposta1 = (3 > 4);
+
+
+std::cout<<resposta<<std::endl;
+std::cout<<resposta1<<std::endl;
+
+return 0;
+
+}
