6a9721df0ae637b7b6deba9a1814004cf70f75d8
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-02-07 13:16:34 -0300

    array com vogal sem sucesso

--- logica.cpp
+++ logica.cpp
@@ -3,16 +3,17 @@
 
 int main(){
 
-std::string vogais[5];
+std::string vogal[5] = {"a", "b", "c", "d","e"};
+std::string letra;
 
-std::cout<<"Digite as 5 vogais em ordem: "<<std::endl;
-std::cin>>vogais[0]>>vogais[1]>>vogais[2]>>vogais[3]>>vogais[4];
+std::cout<<"Digite uma letra: "<<std::endl;
+std::cin>>letra;
 
-std::cout<<vogais[0]<<std::endl;
-std::cout<<vogais[1]<<std::endl;
-std::cout<<vogais[2]<<std::endl;
-std::cout<<vogais[3]<<std::endl;
-std::cout<<vogais[4]<<std::endl;
+if(letra == vogal[5]){
+    std::cout<<"Essa letra e uma vogal!"<<std::endl;
+}else{
+    std::cout<<"Essa letra nao e uma vogal."<<std::endl;
+}
 
 return 0;
 
