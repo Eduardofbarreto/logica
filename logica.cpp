22a79cab1edf04c551fa0cfe13b1da345118d0c3
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-02-07 12:59:47 -0300

    vogais arrays

--- logica.cpp
+++ logica.cpp
@@ -3,12 +3,16 @@
 
 int main(){
 
-std::string nome;
+std::string vogais[5];
 
-std::cout<<"Qual seu nome: ";
-std::cin>>nome;
+std::cout<<"Digite as 5 vogais em ordem: "<<std::endl;
+std::cin>>vogais[0]>>vogais[1]>>vogais[2]>>vogais[3]>>vogais[4];
 
-std::cout<<nome<<std::endl;
+std::cout<<vogais[0]<<std::endl;
+std::cout<<vogais[1]<<std::endl;
+std::cout<<vogais[2]<<std::endl;
+std::cout<<vogais[3]<<std::endl;
+std::cout<<vogais[4]<<std::endl;
 
 return 0;
 
