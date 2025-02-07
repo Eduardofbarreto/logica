ac282240df9cfab40b603be0de4a822fc2a6e9d0
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-02-07 18:42:19 -0300

    pares com numeros maiores

--- logica.cpp
+++ logica.cpp
@@ -3,15 +3,15 @@
 
 int main(){
 
-int numbers [] = {1,2,3,4,5,6,7,8,9,10};
+int numbers [] = {10,20,30,40,50,60,70,80,90,100};
 int tamanho = sizeof(numbers)/sizeof(numbers[0]);
 
-std::cout<<"Numeros pares na lista: "<<std::endl;
     for(int i = 0; i < tamanho; i++){
         if(numbers[i] % 2 == 0){
-            std::cout<<numbers[i]<<" "<<std::endl;
+            std::cout<<"A lista de numeros par e: "<<numbers[i]<<std::endl;
         }
     }
+
 
 
 return 0;
