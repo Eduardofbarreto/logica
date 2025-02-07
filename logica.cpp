12a88e70a6fb36db7559f34c0122240e7bc69bb4
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-02-07 12:56:13 -0300

    string de nome

--- logica.cpp
+++ logica.cpp
@@ -3,19 +3,12 @@
 
 int main(){
 
-int arr1[5], arr2[5], arr3[0];
+std::string nome;
 
-std::cout<<"Digite 5 inteiros para o primeiro array: "<<std::endl;
-std::cin>>arr1[0]>>arr1[1]>>arr1[2]>>arr1[3]>>arr1[4];
+std::cout<<"Qual seu nome: ";
+std::cin>>nome;
 
-std::cout<<"Digite mais 5 inteiros para o outro array: "<<std::endl;
-std::cin>>arr2[0]>>arr2[1]>>arr2[2]>>arr2[3]>>arr2[4];
-
-std::cout<<"Seu primeiro array: "<<arr1[0]<<arr1[1]<<arr1[2]<<arr1[3]<<arr1[4]<<std::endl;
-std::cout<<"Seu segundo array: "<<arr2[0]<<arr2[1]<<arr2[2]<<arr2[3]<<arr2[4]<<std::endl;
-
-
-
+std::cout<<nome<<std::endl;
 
 return 0;
 
