383af5c26cee834cd36f25f564037c748176996b
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-02-07 11:32:59 -0300

    trocando um determinado valor de array para -1

--- logica.cpp
+++ logica.cpp
@@ -2,17 +2,26 @@
 
 int main(){
 
-int arr[3];
-int indice;
+int arr[4];
+int modify;
 
-std::cout<<"Digite 3 numeros inteiros: "<<std::endl;
-std::cin>>arr[0]>>arr[1]>>arr[2];
+std::cout<<"Digite 4 numeros inteiros: "<<std::endl;
+std::cin>>arr[0]>>arr[1]>>arr[2]>>arr[3];
 
-std::cout<<"Digite um valor para selecionar o array: "<<std::endl;
-std::cin>>indice;
+std::cout<<"Digite o indice do array trocado: "<<std::endl;
+std::cin>>modify;
 
-std::cout<<"Voce escolheu: "<<arr[indice]<<std::endl;
+if(modify == 0){
+    arr[0] = -1;
+}else if(modify == 1){
+    arr[1] = -1;
+}else if(modify == 2){
+    arr[2] = -1;
+}else{
+    arr[3] = -1;
+}
 
+std::cout<<"Os arrays ficaram: "<<arr[0]<<" "<<arr[1]<<" "<<arr[2]<<" "<<arr[3]<<std::endl;
 
 return 0;
 
