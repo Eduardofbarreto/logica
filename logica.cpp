e3e867e1ae4517f00d957600c3a91901cecf2e7d
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-02-08 15:44:12 -0300

    dia da semana

--- logica.cpp
+++ logica.cpp
@@ -1,21 +1,25 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 
-int main(){
+int main() {
+    // Declaração do array de caracteres com 7 posições
+    std::string dias[7] = {
+        "Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira",
+        "Quinta-feira", "Sexta-feira", "Sábado"
+    };
 
-int num;
-std::string dias[7] = {"Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira",
-"Quinta-feira", "Sexta-feira", "Sábado"};
+    // Leitura do número do usuário
+    int numero;
+    std::cout << "Digite um número entre 1 e 7: ";
+    std::cin >> numero;
 
-std::cout<<"Digite um numero para ver qual dia corresponde: "<<std::endl;
-std::cin>>num;
-
-    if(num <=0 && num >7){
-        std::cout<<"Numero invalido!"<<std::endl;
-    }else{
-        std::cout<<dias[num-1]<<std::endl;
+    // Verificação se o número está dentro do intervalo válido
+    if (numero >= 1 && numero <= 7) {
+        // Impressão do dia da semana correspondente
+        std::cout << "O dia da semana correspondente é: " << dias[numero - 1] << std::endl;
+    } else {
+        std::cout << "Número inválido. Digite um número entre 1 e 7." << std::endl;
     }
 
-
-return 0;
-
+    return 0;
 }
