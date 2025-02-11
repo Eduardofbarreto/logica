0414c1ce32f37f366cd317343975b045f59bfb12
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-02-11 15:42:35 -0300

    classe palavra

--- logica.cpp
+++ logica.cpp
@@ -1,41 +1,20 @@
 #include <iostream>
-#include<string>
+#include <string>
 
-    class calculadora{
-        public:
-        int a, b;
-        int soma, subt, mult, div;
-    
-
-    void calcular(){
-        soma = a + b;
-        subt = a - b;
-        mult = a * b;
-        if(b != 0){
-            div = a / b;
-        }else{
-            std::cout<<"Erro, divisao por 0!"<<std::endl;
-        }
-    }
+class texto{
+    public:
+    std::string palavra;
 };
 
 int main(){
-    calculadora calc;
 
-    std::cout<<"Digite um valor para a: "<<std::endl;
-    std::cin>>calc.a;
+texto meuTexto;
 
-    std::cout<<"Digite um valor para b: "<<std::endl;
-    std::cin>>calc.b;
+std::cout<<"Digite uma palavra: "<<std::endl;
+std::cin>>meuTexto.palavra;
 
-    calc.calcular();
+std::cout<<"A sua palavra e: "<<meuTexto.palavra<<std::endl;
 
-    std::cout<<"O resultado da soma e: "<<calc.soma<<std::endl;
-    std::cout<<"O resultado da subtracao e: "<<calc.subt<<std::endl;
-    std::cout<<"O resultado da multiplicaçao e: "<<calc.mult<<std::endl;
-    std::cout<<"O resultado da divisao e: "<<calc.div<<std::endl;
-
-
-    return 0;
+return 0;
 
 }
