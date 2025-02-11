06162fdcd16931ac2250a6e964329bddf98b4803
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-02-11 15:19:07 -0300

    calculadora com classe

--- logica.cpp
+++ logica.cpp
@@ -1,12 +1,41 @@
 #include <iostream>
+#include<string>
 
-int main() {
+    class calculadora{
+        public:
+        int a, b;
+        int soma, subt, mult, div;
+    
 
-int a =  2 % 2 == 0;
+    void calcular(){
+        soma = a + b;
+        subt = a - b;
+        mult = a * b;
+        if(b != 0){
+            div = a / b;
+        }else{
+            std::cout<<"Erro, divisao por 0!"<<std::endl;
+        }
+    }
+};
 
-std::cout<<a<<std::endl;
+int main(){
+    calculadora calc;
 
+    std::cout<<"Digite um valor para a: "<<std::endl;
+    std::cin>>calc.a;
+
+    std::cout<<"Digite um valor para b: "<<std::endl;
+    std::cin>>calc.b;
+
+    calc.calcular();
+
+    std::cout<<"O resultado da soma e: "<<calc.soma<<std::endl;
+    std::cout<<"O resultado da subtracao e: "<<calc.subt<<std::endl;
+    std::cout<<"O resultado da multiplicaçao e: "<<calc.mult<<std::endl;
+    std::cout<<"O resultado da divisao e: "<<calc.div<<std::endl;
 
 
     return 0;
+
 }
