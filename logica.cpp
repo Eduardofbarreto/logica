73622414c2f5966f7514ed62b8a8283d3e6a73a2
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-02-11 21:01:31 -0300

    agenda telefonica classe

--- logica.cpp
+++ logica.cpp
@@ -1,48 +1,27 @@
 #include <iostream>
-#include <string>
+#include<string>
 
-class mercado{
+class agenda{
     public:
-    std::string produto1;
-    std::string produto2;
-    std::string produto3;
+    std::string nome;
+    std::string telefone;
 
-    double preco1;
-    double preco2;
-    double preco3;
-
-    double somas(){
-        double resultado = (preco1 + preco2 + preco3);
-        std::cout<<"Voce comprou: "<<produto1<<", "<<produto2<<" e "<<produto3<<std::endl;
-        std::cout<<"Gastando um total de: "<<resultado<<std::endl;
-        return resultado;
-        
+    void mensagem(){
+        std::cout<<"Contato cadastrado com sucesso!"<<std::endl;
     }
 };
 
 int main(){
 
-    mercado mercado;
+agenda cadastra;
 
-    std::cout<<"Digite o produto 1: "<<std::endl;
-    std::cin>>mercado.produto1;
+std::cout<<"Digite o nome: "<<std::endl;
+std::cin>>cadastra.nome;
 
-    std::cout<<"Digite o valor do produto 1: "<<std::endl;
-    std::cin>>mercado.preco1;
+std::cout<<"Digite o telefone: "<<std::endl;
+std::cin>>cadastra.telefone;
 
-    std::cout<<"Digite o produto 2: "<<std::endl;
-    std::cin>>mercado.produto2;
+cadastra.mensagem();
 
-    std::cout<<"Digite o valor do produto 2: "<<std::endl;
-    std::cin>>mercado.preco2;
-
-    std::cout<<"Digite o produto 3: "<<std::endl;
-    std::cin>>mercado.produto3;
-
-    std::cout<<"Digite o valor do produto 3: "<<std::endl;
-    std::cin>>mercado.preco3;
-
-    std::cout<<mercado.somas();
-    
-    return 0;
+  return 0;
 }
