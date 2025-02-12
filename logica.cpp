20fd18c8c523117a9bf543405ba3d6e41fdae442
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-02-11 21:25:35 -0300

    classe cadastro

--- logica.cpp
+++ logica.cpp
@@ -1,27 +1,64 @@
 #include<iostream>
 #include<string>
 
-class agenda{
+class cliente{
     public:
     std::string nome;
     std::string telefone;
+    std::string endereco;
 
-    void mensagem(){
-        std::cout<<"Contato salvo com êxito!";
+    void cadastroCliente(){
+        std::cout<<"Cadastro salvo com êxito!";
     }
 };
 
+class produto{
+    public:
+    std::string nome;
+    int quantidade;
+    float valor;
+
+    void cadastroProduto(){
+        std::cout<<"Produto cadastrado com sucesso!"<<std::endl;
+    }
+};
+
+
+
 int main(){
 
-    agenda processar;
+int opcao;
 
-    std::cout<<"Digite o nome: "<<std::endl;
-    std::cin>>processar.nome;
+cliente inserir;
+produto insert;
+
+std::cout<<"Digite 1 para Cliente ou 2 para produtos: "<<std::endl;
+std::cin>>opcao;
+
+if(opcao == 1){
+    std::cout<<"Digite o nome do cliente: "<<std::endl;
+    std::cin>>inserir.nome;
 
     std::cout<<"Digite o telefone: "<<std::endl;
-    std::cin>>processar.telefone;
+    std::cin>>inserir.telefone;
 
-    processar.mensagem();
+    std::cout<<"Digite o endereco: "<<std::endl;
+    std::cin>>inserir.endereco;
+
+        inserir.cadastroCliente();
+}else{
+    std::cout<<"Digite o nome do produto: "<<std::endl;
+    std::cin>>insert.nome;
+
+    std::cout<<"Digite a quantidade: "<<std::endl;
+    std::cin>>insert.quantidade;
+
+    std::cout<<"Digite o valor: "<<std::endl;
+    std::cin>>insert.valor;
+
+        insert.cadastroProduto();
+}
+
 
 
     return 0;
