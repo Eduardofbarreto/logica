44b4d51415aa5b7665e12fa3c4c88061118b9966
Eduardo <Eduardofbarreto>
2025-02-13 15:27:47 -0300

    vector

--- logica.cpp
+++ logica.cpp
@@ -1,95 +1,21 @@
 #include <iostream>
-#include <string>
-/*
-class calculadora{
-    public:
-    int a, b;
-    int op;
-    float resultado;
-
-    void resolver(){
-        switch(op){
-            case 1:
-                resultado = a + b;
-                std::cout<<a<<" + "<<b<<" = "<<resultado<<std::endl;
-                break;
-            case 2:
-                resultado = a - b;
-                std::cout<<a<<" - "<<b<<" = "<<resultado<<std::endl;
-                break;
-            case 3: 
-                resultado = a * b;
-                std::cout<<a<<" * "<<b<<" = "<<resultado<<std::endl;
-                break;
-            case 4:
-                if(b == 0){
-                    std::cout<<"Erro: Divisao por zero!"<<std::endl;
-                    return;
-                }
-                resultado = static_cast<float>(a)/b;
-                std::cout<<a<<" / "<<b<<" = "<<resultado<<std::endl;
-                break;
-            default:
-                std::cout<<"Operacao invalida!"<<std::endl;
-                return;
-        }
-    }
-};
-*/
-
-class calculadora{
-    public:
-    int a, b;
-    char op;
-    float resultado;
-
-    void calcular(){
-        switch(op){
-            case '+':
-                resultado = a + b;
-                break;
-            case '-':
-                resultado = a - b;
-                break;
-            case '*':
-                resultado = a * b;
-                break;
-            case '/':
-                if(b == 0){
-                    std::cout<<"Erro: Divisao por zero!"<<std::endl;
-                    std::cout<<"\n"<<std::endl;
-                }else{
-                    resultado = static_cast<float>(a) / b;
-                }
-                break;
-            default:
-                std::cout<<"Voce inseriu um valor inexistente!"<<std::endl;
-                return;
-        }
-
-        std::cout<<"\n"<<std::endl;
-        std::cout<<"O resultado de "<<a<<" "<<op<<" "<<b<<" = "<<resultado<<std::endl;
-        std::cout<<"\n"<<std::endl;
-    }
-};
-
+#include <vector>
 
 int main() {
+    std::vector<int> numeros = {10, 20, 30};
+    std::vector<std::string> palavras = {"casa", "comida", "roupa"};
 
-    calculadora calc;
+    /*
+    numeros.push_back(40);
 
-    std::cout<<"Digite um valor para a: "<<std::endl;
-    std::cin>>calc.a;
+    for (int i = 0; i < numeros.size(); i++) {
+        std::cout << numeros[i] << " ";
+    }
+    std::cout << std::endl; // Imprime: 10 20 30 40 50
+    */
 
-    std::cout<<"Digite um valor para operacao: 1(soma), 2(subtracao), 3(multiplicacao) e 4(divisao) "<<std::endl;
-    std::cin>>calc.op;
-
-    std::cout<<"Digite um valor para b: "<<std::endl;
-    std::cin>>calc.b;
-
-
-    calc.calcular();
-
+    std::cout<<numeros.size();
+    std::cout<<palavras.size();
 
     return 0;
 }
