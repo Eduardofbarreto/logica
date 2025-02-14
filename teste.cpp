367b3895448aa6fa26b48e3c9514f4f5670e7c97
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-02-14 19:28:08 -0300

    multiplos de 3 e 5 com classe

--- teste.cpp
+++ teste.cpp
@@ -1,17 +1,29 @@
 #include<iostream>
 #include<string>
 
-int main(){
-    int num;
-
-    std::cout<<"Digite um numero :";
-    std::cin>>num;
-
-    if(num >=0){
-        std::cout<<"Num positivo!";
-    }else{
-        std::cout<<"Num negativo!!";
+class multiplos{
+    public:
+    int num1;
+    multiplos(int num1){
+        this -> num1 = num1;
     }
+    void calcular(){
+        if(num1 % 3 == 0 && num1 % 5 == 0){
+            std::cout<<"O numero "<<this->num1<<" e multiplo de 3 e 5."<<std::endl;
+        }else{
+            std::cout<<"O numero nao e multiplo de 3 e 5."<<std::endl;
+        }
+    }
+};
+
+int main(){
+    int num1;
+
+    std::cout<<"Digite um numero :"<<std::endl;
+    std::cin>>num1;
+
+    multiplos resultado(num1);
+    resultado.calcular();
 
     return 0;
 }
