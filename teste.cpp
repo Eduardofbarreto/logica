7cb30ca5b09b919f6a695a412cc7c5a792d25eb9
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-02-14 16:45:40 -0300

    par ou impar de novo

--- teste.cpp
+++ teste.cpp
@@ -1,27 +1,15 @@
 #include<iostream>
 #include<string>
 
-class pessoa{
-    public:
-    std::string nome;
-
-    pessoa(std::string nome){
-        this->nome = nome;
-    }
-    void imprimirDados(){
-        std::cout<<"Nome: "<<this->nome<<std::endl;
-    }
-};
-
 int main(){
+    int num;
 
-    std::string nome;
+    std::cout<<"Digite algum numero: "<<std::endl;
+    std::cin>>num;
 
-    std::cout<<"Digite um nome: "<<std::endl;
-    std::getline(std::cin>>std::ws,nome);
-
-    pessoa teste(nome);
-    teste.imprimirDados();
-
-    return 0;
+    if(num % 2 == 0){
+        std::cout<<"Par"<<std::endl;
+    }else{
+        std::cout<<"Impar"<<std::endl;
+    }
 }
