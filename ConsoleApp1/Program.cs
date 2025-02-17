44136b85b73a8576dd9ff772381739b25fcbc091
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-02-17 19:52:07 -0300

    classe nome e sobrenome

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,22 +1,20 @@
-﻿public class Cachorro
-{
-    public string Nome { get; set; }
-    public int Idade { get; set; }
+﻿public class pessoa{
+    public string nome {get; set;}
+    public string sobrenome {get; set;}
 }
 
 public class Program
 {
     public static void Main(string[] args)
     {
-        Cachorro meuCachorro = new Cachorro();
+        pessoa cadastro = new pessoa();
 
-        Console.WriteLine("Digite o nome do seu cachorro: ");
-        meuCachorro.Nome = Console.ReadLine();
-        
-        Console.WriteLine("Digite a idade do cachorro: ");
-        meuCachorro.Idade = int.Parse(Console.ReadLine());
+        Console.WriteLine("Digite o nome da pessoa: ");
+        cadastro.nome = Console.ReadLine();
 
-        Console.WriteLine("O nome do cachorro é " + meuCachorro.Nome);
-        Console.WriteLine("A idade do cachorro é " + meuCachorro.Idade);
+        Console.WriteLine("Digite o sobronome: ");
+        cadastro.sobrenome = Console.ReadLine();
+
+        Console.WriteLine("O nome da pessoa é: " + cadastro.nome + " " + cadastro.sobrenome);
     }
 }
