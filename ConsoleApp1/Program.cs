1b3c8a79481e8d489b6bd1237976a52e08f3bcbb
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-02-17 19:55:15 -0300

    Classe com nome, sobrenome e if para idade

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,8 @@
 ﻿public class pessoa{
     public string nome {get; set;}
     public string sobrenome {get; set;}
+
+    public int idade {get; set;}
 }
 
 public class Program
@@ -15,6 +17,16 @@ public class Program
         Console.WriteLine("Digite o sobronome: ");
         cadastro.sobrenome = Console.ReadLine();
 
+        Console.WriteLine("Digite a idade: ");
+        cadastro.idade = int.Parse(Console.ReadLine());
+
+        if(cadastro.idade >= 18){
+            Console.WriteLine("Maior de idade!");
+        }else{
+            Console.WriteLine("Menor de idade!");
+        }
+
         Console.WriteLine("O nome da pessoa é: " + cadastro.nome + " " + cadastro.sobrenome);
+        Console.WriteLine("A idade é: " + cadastro.idade);
     }
 }
