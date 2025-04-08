2a603e5cea95ba0fd3c2c5ce549e766d7501ffae
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-04-08 10:29:23 -0300

    booleana

--- teste.cpp
+++ teste.cpp
@@ -1,23 +1,14 @@
 #include <iostream>
-#include <string>
+#include<cmath>
 
 int main(){
 
     using namespace std;
 
-    string minhaString = "Hello";
-    cout<<minhaString; //saída será um hello normal
-    cout<<"\n";
+    int x = 10;
+    int y = 4;
 
-    cout<<minhaString.at(0); //vai sair o primeiro caractere
-    cout<<"\n";
-    cout<<minhaString.at(1); //vai sair o segundo caractere
-    cout<<"\n";
-    cout<<minhaString.at(minhaString.length() - 1); //último caractere
-    cout<<"\n";
-
-    minhaString.at(0) = 'J';
-    cout<<minhaString; //vai trocar o H pelo J
+    cout<< (x > y);
 
     return 0;
 }
