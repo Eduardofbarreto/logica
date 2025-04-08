c44b50d75cf8568bd20a09dd4f148135104a9a15
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-04-08 09:47:40 -0300

    puxando primeiro, segundo e trocando caracteres

--- teste.cpp
+++ teste.cpp
@@ -6,8 +6,18 @@ int main(){
     using namespace std;
 
     string minhaString = "Hello";
-    cout<<minhaString.length();
+    cout<<minhaString; //saída será um hello normal
     cout<<"\n";
+
+    cout<<minhaString.at(0); //vai sair o primeiro caractere
+    cout<<"\n";
+    cout<<minhaString.at(1); //vai sair o segundo caractere
+    cout<<"\n";
+    cout<<minhaString.at(minhaString.length() - 1); //último caractere
+    cout<<"\n";
+
+    minhaString.at(0) = 'J';
+    cout<<minhaString; //vai trocar o H pelo J
 
     return 0;
 }
