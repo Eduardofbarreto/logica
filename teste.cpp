d142dd151c6136d33a1fe955b316ff640157583a
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-04-08 17:49:06 -0300

    for com matriz

--- teste.cpp
+++ teste.cpp
@@ -3,20 +3,11 @@
 
 int main(){
 
-    std::string nome, sobrenome;
+    std::string cars[5] = {"Volvo", "Bmw", "Ford", "Mazda", "Tesla"};
 
-    std::cout<<"Digite seu nome: "<<std::endl;
-    std::getline(std::cin, nome);
-
-    std::cout<<"Digite seu sobrenome: "<<std::endl;
-    std::getline(std::cin, sobrenome);
-
-    std::cout<<nome<<" "<<std::endl;
-    std::cout<<nome.length()<<std::endl;
-    std::cout<<nome.size()<<std::endl;
-    std::cout<<nome.at(0)<<std::endl;
-    nome.at(0) = 'P';
-    std::cout<<nome<<std::endl;
+    for(int i = 0; i < 5; i++){
+        std::cout<<i<<" = "<<cars[i]<<std::endl;
+    }
  
 
     return 0;
