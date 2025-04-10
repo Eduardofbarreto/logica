6a1163ceef0487252dc9301c5263335a735c5311
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-04-10 09:48:02 -0300

    media das idades com sizeof

--- teste.cpp
+++ teste.cpp
@@ -4,12 +4,19 @@
 
 int main(){
 
-    using namespace std;
-    
-    int myNumbers[5] = {10, 20, 30, 40, 50};
-    for (int i = 0; i < sizeof(myNumbers) / sizeof(myNumbers[0]); i++) {
-      cout << myNumbers[i] << "\n";
-    }
+  int idade[5] = {25, 31, 33, 37, 42};
+
+  float idadeMedia, soma = 0;
+
+  int comprimento = sizeof(idade) / sizeof(idade[0]);
+
+  for(int age : idade){
+    soma += age;
+  }
+
+  idadeMedia = soma / comprimento;
+
+  std::cout<<"A idade media e: "<<idadeMedia<<std::endl;
 
 
     return 0;
