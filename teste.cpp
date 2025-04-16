9552771cbde42d5cca4ccb8005ec7a89446acf65
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-04-16 19:12:49 -0300

    usando struct novamente

--- teste.cpp
+++ teste.cpp
@@ -4,15 +4,28 @@
 int main(){
 
     struct{
-        int myNum;
-        std::string myString;
+        int num1;
+        int num2;
+        std::string text1;
+        std::string text2;
     } myStructure;
 
-    myStructure.myNum = 1;
-    myStructure.myString = "Ola mundo!";
+    std::cout<<"Digite um valor para num 1: "<<std::endl;
+    std::cin>>myStructure.num1;
 
-    std::cout<<myStructure.myNum<<std::endl;
-    std::cout<<myStructure.myString<<std::endl;
+    std::cout<<"Digite um valor para num 2: "<<std::endl;
+    std::cin>>myStructure.num2;
+
+    std::cout<<"Digite uma palavra para texto 1: "<<std::endl;
+    std::cin>>myStructure.text1;
+
+    std::cout<<"Digite um palavra para texto 2: "<<std::endl;
+    std::cin>>myStructure.text2;
+
+    std::cout<<myStructure.num1<<std::endl;
+    std::cout<<myStructure.num2<<std::endl;
+    std::cout<<myStructure.text1<<std::endl;
+    std::cout<<myStructure.text2<<std::endl;
 
     return 0;
 }
