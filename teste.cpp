6ef4f263a95597b18bf1017eb1c5a60fe1bb22d8
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-04-20 17:30:31 -0300

    struct como se fosse uma classe

--- teste.cpp
+++ teste.cpp
@@ -1,26 +1,22 @@
 #include <iostream>
 #include <string>
 
+struct Calculo{
+    int num1, num2;
+    int resultado = num1 + num2;
+};
+
 int main(){
+Calculo meuCalculo;
 
-    struct{
-        int num1, num2, soma;
-        std::string nome;
-    } minhaEstrutura;
+std::cout<<"Digite um valor para num1: "<<std::endl;
+std::cin>>meuCalculo.num1;
 
-    std::cout<<"Digite o seu nome: "<<std::endl;
-    std::cin>>minhaEstrutura.nome;
+std::cout<<"Digite um valor para num2: "<<std::endl;
+std::cin>>meuCalculo.num2;
 
-    std::cout<<"Digite um valor para num1: "<<std::endl;
-    std::cin>>minhaEstrutura.num1;
+std::cout<<"O resultado e: "<<meuCalculo.resultado<<std::endl;
 
-    std::cout<<"Digite um valor para num2: "<<std::endl;
-    std::cin>>minhaEstrutura.num2;
-
-    minhaEstrutura.soma = minhaEstrutura.num1 + minhaEstrutura.num2;
-
-    std::cout<<"Seu nome e: "<<std::endl;
-    std::cout<<"O resultado da soma e: "<<minhaEstrutura.soma<<std::endl;
 
 return 0;
 }
