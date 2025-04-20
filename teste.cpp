bd0fe38a8f27701bb54769cdd1529fa71c8fefea
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-04-19 22:12:22 -0300

    trabalhando mais com structs

--- teste.cpp
+++ teste.cpp
@@ -4,28 +4,18 @@
 int main(){
 
     struct{
-        int num1;
-        int num2;
-        std::string text1;
-        std::string text2;
-    } myStructure;
+        int num1, num2, soma;
+    } minhaEstrutura;
 
-    std::cout<<"Digite um valor para num 1: "<<std::endl;
-    std::cin>>myStructure.num1;
+    std::cout<<"Digite um valor para num1: "<<std::endl;
+    std::cin>>minhaEstrutura.num1;
 
-    std::cout<<"Digite um valor para num 2: "<<std::endl;
-    std::cin>>myStructure.num2;
+    std::cout<<"Digite um valor para num2: "<<std::endl;
+    std::cin>>minhaEstrutura.num2;
 
-    std::cout<<"Digite uma palavra para texto 1: "<<std::endl;
-    std::cin>>myStructure.text1;
+    minhaEstrutura.soma = minhaEstrutura.num1 + minhaEstrutura.num2;
 
-    std::cout<<"Digite um palavra para texto 2: "<<std::endl;
-    std::cin>>myStructure.text2;
-
-    std::cout<<myStructure.num1<<std::endl;
-    std::cout<<myStructure.num2<<std::endl;
-    std::cout<<myStructure.text1<<std::endl;
-    std::cout<<myStructure.text2<<std::endl;
+    std::cout<<"O resultado e: "<<minhaEstrutura.soma<<std::endl;
 
     return 0;
 }
