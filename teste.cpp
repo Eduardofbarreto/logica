e22bb09cf038e059d726054f253d09ef066b81be
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-04-21 13:36:09 -0300

    references part II

--- teste.cpp
+++ teste.cpp
@@ -3,11 +3,19 @@
 
 int main(){
 
-    std::string nome = "Edu";
-    std::string &eu = nome;
+    int num1, num2, soma;
+    int calc1 = num1; 
+    int calc2 = num2;
+    
+        std::cout<<"Digite um valor para num1: "<<std::endl;
+        std::cin>>calc1; 
+    
+        std::cout<<"Digite um valor para num2: "<<std::endl;
+        std::cin>>calc2;    
 
-    std::cout<<eu<<std::endl;
+        soma = calc1 + calc2;
 
+        std::cout<<"O resultado e: "<<soma<<std::endl;
 
     return 0;
 }
