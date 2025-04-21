038789226726e8ea27e1330c27a3318ebfc3fd8a
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-04-21 13:44:04 -0300

    espaco da memoria usado para guardar a variavel

--- teste.cpp
+++ teste.cpp
@@ -1,21 +1,11 @@
 #include <iostream>
-#include <string>
+
 
 int main(){
 
-    int num1, num2, soma;
-    int calc1 = num1; 
-    int calc2 = num2;
-    
-        std::cout<<"Digite um valor para num1: "<<std::endl;
-        std::cin>>calc1; 
-    
-        std::cout<<"Digite um valor para num2: "<<std::endl;
-        std::cin>>calc2;    
+    int num = 4;
 
-        soma = calc1 + calc2;
-
-        std::cout<<"O resultado e: "<<soma<<std::endl;
+    std::cout<<&num<<std::endl;
 
     return 0;
 }
