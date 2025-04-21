7a258eccc938f460212c54bb3ac25bb1d298887e
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-04-21 13:30:20 -0300

    references

--- teste.cpp
+++ teste.cpp
@@ -1,32 +1,12 @@
-#include<iostream>
-#include<string>
-
-class Padrao{
-    public:
-    std::string nome;
-    int idade;
-
-    Padrao(){}
-
-    virtual void gravarInfo(){
-        std::cout<<"Qual seu nome: "<<std::endl;
-        std::getline(std::cin, nome);
-        std::cout<<"Qual sua idade: "<<std::endl;
-        std::cin>>idade;
-    }
-
-    virtual void exibirInfor(){
-        std::cout<<"O nome e: "<<nome<<"."<<std::endl;
-        std::cout<<"A idade e: "<<idade<<"."<<std::endl;
-    }
-};
+#include <iostream>
+#include <string>
 
 int main(){
 
-    Padrao meuPadrao;
-    meuPadrao.gravarInfo();
-    meuPadrao.exibirInfor();
+    std::string nome = "Edu";
+    std::string &eu = nome;
 
+    std::cout<<eu<<std::endl;
 
 
     return 0;
