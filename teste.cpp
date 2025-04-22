d43defcd374605c0210058106ea244f83de02bf5
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-04-22 10:41:38 -0300

    seguindo com funcoes

--- teste.cpp
+++ teste.cpp
@@ -1,18 +1,23 @@
 #include <iostream>
 #include<string>
 
-void Aprensentacao(std::string nome){
+void Aprensentacao(std::string nome, int idade){
     std::cout<<"Bem-vindo "<<nome<<"!"<<std::endl;
+    std::cout<<"Voce tem "<<idade<<" anos!"<<std::endl;
 }
 
 int main(){
 
     std::string nome;
+    int idade;
 
     std::cout<<"Digite seu nome: "<<std::endl;
     std::cin>>nome;
 
-    Aprensentacao(nome);
+    std::cout<<"Digite a sua idade: "<<std::endl;
+    std::cin>>idade;
+
+    Aprensentacao(nome, idade);
 
     return 0;
 }
