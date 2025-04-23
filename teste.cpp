5aabcc1888d9b120c292ab45f3f05ff826cee2ca
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-04-23 13:51:04 -0300

    funcoes com argumentos

--- teste.cpp
+++ teste.cpp
@@ -1,43 +1,16 @@
 #include <iostream>
 #include<string>
 
-class Carro{
-    public:
-    std::string nome;
-    std::string cor;
-
-    Carro(){}
-
-    void Acelerar(){
-        std::cout<<"O "<<nome<<" "<<cor<<" esta andando a 40km/h."<<std::endl;
-    }
-
-    void Frear(){
-        std::cout<<"O "<<nome<<" "<<cor<<" esta parando."<<std::endl;
-    }
-};
+void minhaFuncao(std::string nomes){
+    std::cout<<nomes<<std::endl;
+}
 
 int main(){
 
-    int op;
-
-    Carro meuCarro;
-
-    std::cout<<"Digite o nome do carro: "<<std::endl;
-    std::cin>>meuCarro.nome;
-
-    std::cout<<"Digite a cor do carro: "<<std::endl;
-    std::cin>>meuCarro.cor;
-
-    std::cout<<"Digite 1 ou 2: "<<std::endl;
-    std::cin>>op;
-
-    if(op == 1){
-        meuCarro.Acelerar();
-    }else{
-        meuCarro.Frear();
-    }
-
+    minhaFuncao("Eduardo");
+    minhaFuncao("Casa");
+    minhaFuncao("teste");
+    minhaFuncao("Olaaaaaaa!!");
 
     return 0;
 }
