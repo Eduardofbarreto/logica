6eebcba66e88e32dbab50af6a532ab426d92540e
Eduardo.fbarreto <fbarreto.eduardo@gmail.com>
2025-04-30 15:20:15 -0300

    class

--- teste.cpp
+++ teste.cpp
@@ -1,18 +1,21 @@
 #include<iostream>
 #include<string>
 
-class Car{
+class myClass{
     public:
-        int speed(int maxSpeed);
+    int num1 = 5;
+    int num2 = 2;
+    void myMethod();
 };
 
-int Car::speed(int maxSpeed){
-    return maxSpeed;
+void myClass::myMethod(){
+    std::cout<<num1<<" + "<<num2<<" = "<<num1 + num2<<std::endl;
 }
 
 int main(){
-    Car myObj;
-    std::cout<<myObj.speed(200);
+
+    myClass Obj1;
+    Obj1.myMethod();
 
     return 0;
 }
