1adb27f1d4b8b13341a05955e62ab93cf06a7fd4
Eduardo.fbarreto <fbarreto.eduardo@gmail.com>
2025-04-30 15:32:18 -0300

    construtor com parametros

--- teste.cpp
+++ teste.cpp
@@ -1,16 +1,24 @@
 #include<iostream>
 #include<string>
 
-class MyClass{
+class Car{
     public:
-        MyClass(){
-            std::cout<<"Olá mundo!";
-        }
+    std::string brand;
+    std::string model;
+    int year;
+
+    Car(std::string x, std::string y, int z){
+        brand = x;
+        model = y;
+        year = z;
+    }
 };
 
 int main(){
-    
-    MyClass myObj;
-    
-    return 0;
+
+    Car carObj1("BMW", "X5", 1999);
+    Car carObj2("Ford", "Mustang", 1969);
+
+    std::cout<<carObj1.brand<<" "<<carObj1.model<<" "<<carObj1.year<<std::endl;
+    std::cout<<carObj2.brand<<" "<<carObj2.model<<" "<<carObj2.year<<std::endl;
 }
