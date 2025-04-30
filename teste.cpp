60de3b40d55bad6f7ec254862a0deb2031c01c40
Eduardo.fbarreto <fbarreto.eduardo@gmail.com>
2025-04-30 16:45:58 -0300

    um tipo de contrutor

--- teste.cpp
+++ teste.cpp
@@ -3,40 +3,47 @@
 #include<iomanip>
 #include<locale>
 
-class Produto{
+class Carro{
     public:
-    std::string nome;
-    std::string tipo;
+    std::string marca;
+    std::string modelo;
+    int ano;
     double valor;
 
-    Produto(std::string n, std::string t, double v){
-        nome = n;
-        tipo = t;
+    Carro(std::string m, std::string mod, int a, double v){
+        marca = m;
+        modelo = mod;
+        ano = a;
         valor = v;
     }
 };
 
 int main(){
 
-    std::string pNome;
-    std::string pTipo;
-    double pValor;
+    std::string cMarca;
+    std::string cModelo;
+    int cAno;
+    double cValor;
 
-    std::cout<<"Digite o nome do produto: "<<std::endl;
-    std::getline(std::cin, pNome);
+    std::cout<<"Digite a marca do veículo: "<<std::endl;
+    std::cin>>cMarca;
 
-    std::cout<<"Digite o tipo do produto: "<<std::endl;
-    std::cin>>pTipo;
+    std::cout<<"Digite o modelo do veículo: "<<std::endl;
+    std::cin>>cModelo;
 
-    std::cout<<"Digite o valor do produto: "<<std::endl;
-    std::cin>>pValor;
+    std::cout<<"Digite o ano do veículo: "<<std::endl;
+    std::cin>>cAno;
 
-    Produto meuProduto(pNome, pTipo, pValor);
+    std::cout<<"Digite o valor do veículo: "<<std::endl;
+    std::cin>>cValor;
 
-    std::cout<<"\nInformações de cadastro: "<<std::endl;
-    std::cout<<"Nome do produto: "<<meuProduto.nome<<std::endl;
-    std::cout<<"Tipo de produto: "<<meuProduto.tipo<<std::endl;
-    std::cout<<"Valor do produto: R$ "<<std::fixed<<std::setprecision(2)<<meuProduto.valor<<std::endl;
+    Carro(cMarca, cModelo, cAno, cValor);
+
+    std::cout<<"\nInformações sobre o veículo: "<<std::endl;
+    std::cout<<"Marca: "<<cMarca<<std::endl;
+    std::cout<<"Modelo: "<<cModelo<<std::endl;
+    std::cout<<"Ano: "<<cAno<<std::endl;
+    std::cout<<"Valor: R$"<<std::fixed<<std::setprecision(2)<<cValor<<std::endl;
     std::cout.imbue(std::locale("pt_BR.UTF-8"));
     std::cout.imbue(std::locale("C"));
     std::cout<<"\n"<<std::endl;
