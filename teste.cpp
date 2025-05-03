15431642b4ccc0ea10b84c92188c6afc4109585a
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-05-03 19:22:29 -0300

    construtores vazio e com paramêtros

--- teste.cpp
+++ teste.cpp
@@ -5,9 +5,8 @@ class Calculo{
     public:
     int num1, num2, soma;
 
-    Calculo(int a, int b) : num1(a), num2(b), soma(0){}
-
-    /*
+    Calculo(){}
+   
     void ColetarDados(){
         std::cout<<"Digite um valor para num1: "<<std::endl;
         std::cin>>num1;
@@ -15,7 +14,7 @@ class Calculo{
         std::cout<<"Digite um valor para num2: "<<std::endl;
         std::cin>>num2;
     }
-    */
+
 
     void Resolver(){
         soma = num1 + num2;
@@ -25,8 +24,8 @@ class Calculo{
 
 int main(){
 
-    Calculo meuObj(3,2);
-    //meuObj.ColetarDados();
+    Calculo meuObj;
+    meuObj.ColetarDados();
     meuObj.Resolver();
 
     return 0;
