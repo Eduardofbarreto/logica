00cbb967015467fe0e26bc20fa38ed325fc9e510
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-05-03 21:48:34 -0300

    construtor com paramtros

--- teste.cpp
+++ teste.cpp
@@ -1,26 +1,32 @@
 #include<iostream>
 #include<string>
+#include<locale>
+#include<iomanip>
 
-class Pessoa{
+class Produto{
     public:
     std::string nome;
-    int idade;
+    double valor;
 
-    Pessoa(std::string nomeInicial, int idadeInicial) :
-        nome(nomeInicial), idade(idadeInicial){}
+    Produto(std::string n, double v) :
+        nome(n), valor(v) {}
 
-    void apresentar() const{
-        std::cout<<"Olá, meu nome é "<<nome<<" e tenho "<<idade<<" anos."<<std::endl;
+    void informacoes(){
+        std::cout<<"Nome do produto "<<nome<<std::endl;
+        std::cout<<"Preço: R$ "<<std::fixed<<std::setprecision(2)<<valor<<std::endl;
+        std::cout.imbue(std::locale("pt_BR.UTF-8"));
+        std::cout.imbue(std::locale("C"));
     }
 };
 
 int main(){
 
-    Pessoa pessoa1("Alice", 30);
-    pessoa1.apresentar();
+    Produto produto1("Garrafa", 4.69);
+    produto1.informacoes();
 
-    Pessoa pessoa2("Carlos", 26);
-    pessoa2.apresentar();
+    Produto produto2("Mouse", 669.89);
+    produto2.informacoes();
+
 
     return 0;
 }
