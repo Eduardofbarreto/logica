65d7b4234971093fbb9edec95fef5a17cf8878ac
profefbarreto <profefbarreto@gmail.com>
2025-05-05 20:49:10 -0300

    tentando entender denovo

--- teste.cpp
+++ teste.cpp
@@ -3,46 +3,34 @@
 #include<iomanip>
 #include<locale>
 
-class Carro{
+class Produto{
     public:
-    std::string marca;
-    std::string modelo;
-    int ano;
+    std::string nome;
     double valor;
 
-    Carro(){}
+    Produto(){}
 
-    void preencher(){
-        std::cout<<"Digite a marca do veículo: "<<std::endl;
-    std::cin>>marca;
-
-    std::cout<<"Digite o modelo do veículo: "<<std::endl;
-    std::cin>>modelo;
-
-    std::cout<<"Digite o ano do veículo: "<<std::endl;
-    std::cin>>ano;
-
-    std::cout<<"Digite o valor do veículo: "<<std::endl;
-    std::cin>>valor;
+    void coletarInfo(){
+        std::cout<<"Digite o nome do produto: "<<std::endl;
+        std::cin>>nome;
+        std::cout<<"Digite o valor do produto: "<<std::endl;
+        std::cin>>valor;
     }
 
-    void exibir(){
-        std::cout<<"\nInformações sobre o veículo: "<<std::endl;
-        std::cout<<"Marca: "<<marca<<std::endl;
-        std::cout<<"Modelo: "<<modelo<<std::endl;
-        std::cout<<"Ano: "<<ano<<std::endl;
-        std::cout<<"Valor: R$"<<std::fixed<<std::setprecision(2)<<valor<<std::endl;
+    void exibirInfo(){
+        std::cout<<"O nome do produto é: "<<nome<<std::endl;
+        std::cout<<"O valor do produto é: R$ "<<std::fixed<<std::setprecision(2)<<valor<<std::endl;
         std::cout.imbue(std::locale("pt_BR.UTF-8"));
         std::cout.imbue(std::locale("C"));
-        std::cout<<"\n"<<std::endl;
     }
 };
 
 int main(){
 
-    Carro meuCarro;
-    meuCarro.preencher();
-    meuCarro.exibir();
+    Produto meuObj;
+    meuObj.coletarInfo();
+    meuObj.exibirInfo();
 
     return 0;
 }
+    
