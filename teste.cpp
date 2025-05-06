33842413e17c593e2949075edc3b7285a252f00f
profefbarreto <profefbarreto@gmail.com>
2025-05-06 10:55:29 -0300

    construtor com parâmetros tentando entender essa bagaça

--- teste.cpp
+++ teste.cpp
@@ -1,35 +1,28 @@
 #include <iostream>
 #include <string>
-#include<iomanip>
-#include<locale>
 
-class Produto{
+class Numbers{
     public:
-    std::string nome;
-    double valor;
+    int a;
+    int b;
 
-    Produto(){}
+    Numbers(int num1, int num2) : a(num1), b(num2){}
 
-    void coletarInfo(){
-        std::cout<<"Digite o nome do produto: "<<std::endl;
-        std::cin>>nome;
-        std::cout<<"Digite o valor do produto: "<<std::endl;
-        std::cin>>valor;
+    void Calcular(){
+        int resultado = a * b;
+        std::cout<<"O resultado é: "<<resultado<<std::endl;
     }
 
-    void exibirInfo(){
-        std::cout<<"O nome do produto é: "<<nome<<std::endl;
-        std::cout<<"O valor do produto é: R$ "<<std::fixed<<std::setprecision(2)<<valor<<std::endl;
-        std::cout.imbue(std::locale("pt_BR.UTF-8"));
-        std::cout.imbue(std::locale("C"));
-    }
 };
 
 int main(){
 
-    Produto meuObj;
-    meuObj.coletarInfo();
-    meuObj.exibirInfo();
+    int aNum1 = 4;
+    int aNum2 = 2;
+
+    Numbers meuObj(aNum1, aNum2);
+    meuObj.Calcular();
+
 
     return 0;
 }
