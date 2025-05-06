541827ea1bdc92430e2835d3aa634be0de34c1d1
profefbarreto <profefbarreto@gmail.com>
2025-05-06 14:31:13 -0300

    classe com array

--- teste.cpp
+++ teste.cpp
@@ -1,30 +1,32 @@
 #include <iostream>
 #include <string>
-#include<iomanip>
-#include<locale>
 
-class Produtos{
+class Indice{
     public:
-    int qtd;
-    double valor, resultado;
+    int num[3] = {1,2,3};
+    int numbers[5] = {10, 20, 30, 40, 50};
+    int numA, numbersA;
 
-    Produtos(){}
+    Indice(){}
 
-    void Calcular(){
-        resultado = qtd * valor;
-        std::cout<<"O resultado é: R$"<<resultado<<std::fixed<<std::setprecision(2)<<std::endl;
-        std::cout.imbue(std::locale("pt_BR.UTF-8"));
-        std::cout.imbue(std::locale("C"));
+    void coletarDados(){
+        std::cout<<"Digite um valor para num: "<<std::endl;
+        std::cin>>numA;
+        std::cout<<"Digite um valor para numbers: "<<std::endl;
+        std::cin>>numbersA;
+    }
+
+    void exibirDados(){
+        std::cout<<num[numA]<<std::endl;
+        std::cout<<numbers[numbersA]<<std::endl;
     }
 };
 
 int main(){
 
-    Produtos meuObj;
-    meuObj.qtd = 6;
-    meuObj.valor = 9.99;
-
-    meuObj.Calcular();
+    Indice meuObj;
+    meuObj.coletarDados();
+    meuObj.exibirDados();
 
     return 0;
 }
