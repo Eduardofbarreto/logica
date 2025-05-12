69d24ffe538489b1fa43056200b2de8b016d8339
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-05-12 10:08:44 -0300

    herança

--- teste.cpp
+++ teste.cpp
@@ -14,11 +14,21 @@ class Carro : public Veiculo{
     std::string modelo = "Mustang";
 };
 
+class Carro1 : public Veiculo{
+    public:
+    std::string modelo = "Ranger";
+};
+
 int main(){
 
     Carro meuCarro;
+    Carro1 meuCarro1;
+
     meuCarro.barulho();
-    std::cout<<meuCarro.modelo + " " + meuCarro.modelo;
+    std::cout<<meuCarro.marca + " " + meuCarro.modelo<<std::endl;
+
+    meuCarro1.barulho();
+    std::cout<<meuCarro1.marca + " " + meuCarro1.modelo<<std::endl;
 
     return 0;
 }
