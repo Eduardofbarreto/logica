83793b1ce10ca5f939e0909c5e80e2f464d1a372
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-05-12 10:06:00 -0300

    herança

--- teste.cpp
+++ teste.cpp
@@ -1,24 +1,24 @@
 #include<iostream>
 #include<string>
-#include<locale>
-#include<iomanip>
+
+class Veiculo{
+    public:
+    std::string marca = "Ford";
+    void barulho(){
+        std::cout<<"Tuu Tuuu"<<std::endl;
+    }
+};
+
+class Carro : public Veiculo{
+    public:
+    std::string modelo = "Mustang";
+};
 
 int main(){
 
-    std::string nome;
-    double valor;
-
-    std::cout<<"Qual nome do produto? "<<std::endl;
-    std::cin>>nome;
-
-    std::cout<<"Qual valor do produto? "<<std::endl;
-    std::cin>>valor;
-
-    std::cout<<"O nome do produto é: "<<nome<<std::endl;
-    std::cout<<"O valor do produto é: R$ "<<std::fixed<<std::setprecision(2)<<valor<<std::endl;
-    std::cout.imbue(std::locale("pt_BR.UTF-8"));
-    std::cout.imbue(std::locale("C"));
-
+    Carro meuCarro;
+    meuCarro.barulho();
+    std::cout<<meuCarro.modelo + " " + meuCarro.modelo;
 
     return 0;
 }
