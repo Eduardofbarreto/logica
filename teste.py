d3e63b229bd4039e2c64c1c4bb1b479b78c3a2c6
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-05-17 10:46:36 -0300

    dados

--- teste.py
+++ teste.py
@@ -1,1 +1,13 @@
-print("Olá")
+nome = "Eduardo"
+sobrenome = input("Digite o sobrenome: ")
+
+a = int(input("Digite um valor para a: "))
+b = int(input("Digite um valor para b: "))
+c = float(input("Digite um valor para c: "))
+
+soma = (a + b)*c
+
+
+
+print(nome, sobrenome)
+print("O resultado é igual a ", soma)
