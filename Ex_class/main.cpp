aa433bb7ce078639c8b678be410e6c40de9af7bf
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-05-21 10:03:35 -0300

    lógica de switch dando erro

--- Ex_class/main.cpp
+++ Ex_class/main.cpp
@@ -1,11 +1,35 @@
 #include "RacaoCaes.h"
+#include "Semente.h"
+#include<iostream>
 
 int main(){
 
-    RacaoCaes obj;
-    obj.coletar();
-    obj.calcular();
-    obj.exibir();
+    int opcao;
+
+    std::cout<<"Escolha a opção adquada: "<<std::endl;
+    std::cout<<"1 - Rações "<<std::endl;
+    std::cout<<"2 - Sementes "<<std::endl;
+    std::cin>>opcao;
+
+
+    switch(opcao){
+        case 1:
+            RacaoCaes obj;
+            obj.coletar();
+            obj.calcular();
+            obj.exibir();
+        break;
+        case 2:
+            Sementes obj1;
+            obj1.coletar();
+            obj1.calcular();
+            obj1.exibir();
+        break;
+        default:
+            std::cout<<"Nenhuma opção válida!"
+    }
+
+    
 
     return 0;
 }
