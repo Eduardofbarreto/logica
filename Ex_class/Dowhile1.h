cb9128f7a71f64b62d5fccd8225441694e36def0
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-05-22 21:59:02 -0300

    tentando

---
+++ Ex_class/Dowhile1.h
@@ -0,0 +1,14 @@
+#ifndef DOWHILE1_H
+#define DOWHILE1_H
+#include<string>
+
+class Dowhile1{
+    public:
+    std::string user;
+    std::string password;
+
+    void autenticar();
+    void naoAutenticar();
+};
+
+#endif
