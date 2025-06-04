fc7645335635c3c3b91a1c323bb9c8d6b882cb31
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-06-04 17:46:24 -0300

    relembrando o do while

---
+++ Ex_class/Strings.h
@@ -0,0 +1,10 @@
+#ifndef STRINGS_H
+#define STRINGS_H
+#include<string>
+
+class Strings{
+    public:
+    std::string palavra;
+    std::string nome;
+    std::string sobrenome;
+}
--- teste.cpp
+++ teste.cpp
@@ -1,73 +1,25 @@
-#include <iostream>
-#include <string>
-#include<iomanip>
-#include<locale>
-
-class Imovel{
-    public:
-        double largura;
-        double comprimento;
-        double area;
-        const double vMetro = 16000;
-        double valorImovel;
-    
-    Imovel(){}
-
-    void calculo(){
-        area = largura * comprimento;
-    }
-
-    virtual void calcValor(){
-        valorImovel = vMetro * area;
-        std::cout<<"O valor do imóvel é de: R$ "<<
-            std::fixed<<std::setprecision(2)<<valorImovel<<std::endl;
-        std::cout<<std::endl;
-    }
-
-    virtual void coleta(){
-        std::cout<<"Qual a largura do imóvel: "<<std::endl;
-        std::cin>>largura;
-        std::cout<<"Qual o comprimento do imóvel: "<<std::endl;
-        std::cin>>comprimento;
-        std::cout<<std::endl;
-    }
-
-    virtual void informacoes(){
-        std::cout<<"O imóvel tem "<<largura<<"metros de largura."<<std::endl;
-        std::cout<<"O imóvel tem "<<comprimento<<"metros de comprimento."<<std::endl;
-        std::cout<<"O imóvel possui "<<area<<" metros quadrados."<<std::endl;
-        std::cout<<std::endl;
-    }
-};
-
-class Casa : public Imovel{
-    public:
-    int quartos;
-    int banheiros;
-
-    Casa(){}
-
-    void coleta() override{
-        Imovel::coleta();
-        std::cout<<"Quantos quartos possui o imóvel: "<<std::endl;
-        std::cin>>quartos;
-        std::cout<<"Quantos banheiros possui o imóvel: "<<std::endl;
-        std::cin>>banheiros;
-    }
-
-    void informacoes() override{
-        Imovel::informacoes();
-        std::cout<<"O imóvel possui "<<quartos<<" quarto(s)."<<std::endl;
-        std::cout<<"O imóvel possui "<<banheiros<<" banheiro(s)."<<std::endl;
-        Imovel::calcValor();
-    }
-};
+#include<iostream>
+#include<string>
+#include<vector>
+#include<limits>
 
 int main(){
 
-    Casa minhaCasa;
-    minhaCasa.coleta();
-    minhaCasa.informacoes();
+    int num_sec = 8;
+    int tentativas;
+    int num;
+
+    do{
+        std::cout<<"Tente a sorte: "<<std::endl;
+        std::cout<<"Digite um número entre 1 a 100: "<<std::endl;
+        std::cin>>num;
+    }while(num != num_sec);
+        if(num == num_sec){
+            std::cout<<"Parabéns! Você acertou!"<<num_sec<<"!"<<std::endl;
+        }else{
+            std::cout<<"Continue tentando!"<<std::endl;
+            std::cout<<"Digite um número de 1 a 100: "<<std::endl;
+        }
 
     return 0;
 }
