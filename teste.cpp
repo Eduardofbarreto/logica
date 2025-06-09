286970fa19f541bbbe64ce8ec221cb9358e37819
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-06-09 16:09:50 -0300

    interagindo com postgre, bando simples

--- teste.cpp
+++ teste.cpp
@@ -1,24 +1,105 @@
 #include<iostream>
-#include<string>
+#include<pqxx/pqxx>
 
-class Somar{
-    public:
-    int a = 4;
-    int b = 3;
-    int soma;
+void inserirPessoa(pqxx::connection &conn){
+    std::string nome;
+    int idade;
 
-    Somar(){}
+    std::cout<<"Nome: "<<std::endl;
+    std::cin.ignore();
+    std::getline(std::cin, nome);
+    std::cout<<"Idade: "<<std::endl;
+    std::cin>>idade;
 
-    void calc(){
-        soma = a + b;
-        std::cout<<"O resultado é: "<<soma<<std::endl;
+    pqxx::work txn(conn);
+    txn.exec_params("INSERT INTO pessoas (nome, idade) VALUES ($1, $2)", nome, idade);
+    txn.commit();
+
+    std::cout<<"Pessoa inserida com sucesso!"<<std::endl;
+
+}
+
+void listarPessoa(pqxx::connection &conn){
+    pqxx::nontransaction txn(conn);
+    pqxx::result r = txn.exec("SELECT id, nome, idade FROM pessoas ORDER BY id");
+
+    std::cout<<"\n --- Lista de pessoas --- \n";
+    for (const auto &row : r){
+        std::cout<<"ID: "<<row["id"].as<int>()
+                 <<", Nome: "<<row["nome"].as<std::string>()
+                 <<", Idade: "<<row["idade"].as<int>()<<"\n";
     }
-};
+}
+
+void atualizarPessoa(pqxx::connection &conn){
+    int id;
+    std::string nome;
+    int idade;
+
+    std::cout<<"ID d apessoa para atualizar: "<<std::endl;
+    std::cin>>id;
+    std::cin.ignore();
+
+    std::cout<<"Novo nome: "<<std::endl;
+    std::getline(std::cin, nome);
+    std::cout<<"Nova idade: "<<std::endl;
+    std::cin>>idade;
+
+    pqxx::work txn(conn);
+    txn.exec_params("UPDATE pessoas SET nome=$1, idade=$2 WHERE id=$3", nome, idade, id);
+    txn.commit();
+
+    std::cout<<"Pessoa atualizada com sucesso!"<<std::endl;
+}
+
+void deletarPessoa(pqxx::connection &conn){
+    int id;
+    std::cout<<"ID da pessoa para deletar: "<<std::endl;
+    std::cin>>id;
+
+    pqxx::work txn(conn);
+    txn.exec_params("DELETE FROM pessoas WHERE id=$1", id);
+    txn.commit();
+
+    std::cout<<"Pessoa deletada com sucesso!"<<std::endl;
+}
 
 int main(){
 
-    Somar s;
-    s.calc();
+    try{
+        pqxx::connection conn("dbname=meu_novo_banco_de_dados user=postgres password=root host=localhost");
+
+        if(!conn.is_open()){
+            std::cerr<<"Erro ao conectar no banco de dados!"<<std::endl;
+            return 1;
+        }
+
+        int opcao;
+        do{
+            std::cout<<"\n ===== Menu CRUD pessoas ====="<<std::endl;
+            std::cout<<"1. Inserir pessoa"<<std::endl;
+            std::cout<<"2. Listar pessoas"<<std::endl;
+            std::cout<<"3. Atualizar pessoa"<<std::endl;
+            std::cout<<"4. Deletar pessoa"<<std::endl;
+            std::cout<<"0. Sair"<<std::endl;
+            std::cin>>opcao;
+
+            switch(opcao){
+                case 1: inserirPessoa(conn);break;
+                case 2: listarPessoa(conn);break;
+                case 3: atualizarPessoa(conn);break;
+                case 4: deletarPessoa(conn);break;
+                case 0: std::cout<<"Saindo...\n";break;
+                default:std::cout<<"Opção inválida!\n";break;
+            }
+        }while(opcao != 0);
+
+        conn.disconnect();
+    }catch (const std::exception &e){
+        std::cerr<<"Erro "<<e.what()<<std::endl;
+        return 1;
+    }
 
     return 0;
+
 }
--- teste.py
+++ teste.py
@@ -1,11 +1,84 @@
-class Person:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+import psycopg2
+
+def inserir_pessoa(conn):
+    nome = input("Nome: ")
+    idade = int(input("Idade: "))
+    
+    with conn.cursor() as cur:
+        cur.execute("INSERT INTO pessoas (nome, idade) VALUES (%s, %s)", (nome, idade))
+        conn.commit()
         
-    def myfunc(self):
-        print("Hello, my name is " + self.name)
+    print("Pessoa inserida com sucesso!")
+    
+def listar_pessoas(conn):
+    with conn.cursor() as cur:
+        cur.execute("SELECT id, nome, idade FROM pessoas ORDER BY id")
+        resultados = cur.fetchall()
         
-p1 = Person("John", 36)
-p1.myfunc()
+        print("\n ----- Lista de pessoas -----")
+        for row in resultados:
+            print(f"ID: {row[0]}, Nome: {row[1]}, Idade: {row[2]}")
+            
+def atualizar_pessoa(conn):
+    id_pessoa = int(input("ID da pessoa para atualizar: "))
+    nome = input("Novo nome: ")
+    idade = int(input("Nova idade: "))
+    
+    with conn.cursor() as cur:
+        cur.execute(
+            "UPDATE pessoas SET nome=%s, idade=%s WHERE id=%s",
+            (nome, idade, id_pessoa)
+        )
+        conn.commit()
         
+        print("Pessoa atualizada com sucesso!")
+        
+def deletar_pessoa(conn):
+    id_pessoa = int(input("ID da pessoa para deletar: "))
+    
+    with conn.cursor() as cur:
+        cur.execute("DELETE FROM pessoas WHERE id=%s", (id_pessoa))
+        conn.commit()
+        
+    print("Pessoa deletada com sucesso!")
+    
+def main():
+    try:
+        conn = psycopg2.connect(
+            dbname="meu_novo_banco_de_dados",
+            user="postgres",
+            password="root",
+            host="localhost"
+        )
+        
+        while True:
+            print("\n ===== Menu CRUD pessoas =====")
+            print("1. Inserir pessoa")
+            print("2. Listar pessoas")
+            print("3. Atualizar pessoa")
+            print("4. Deletar pessoa")
+            print("0. Sair")
+            
+            opcao = input("Escolha uma opção: ")
+            
+            if opcao == '1':
+                inserir_pessoa(conn)
+            elif opcao == '2':
+                listar_pessoas(conn)
+            elif opcao == '3':
+                atualizar_pessoa(conn)
+            elif opcao == '4':
+                deletar_pessoa(conn)
+            elif opcao == '0':
+                print("Saindo...")
+                break
+            else:
+                print("Opção inválida!")
+                
+        conn.close()
+        
+    except Exception as e:
+        print("Erro: ", e)
+        
+if __name__ == "__main__":
+    main()
