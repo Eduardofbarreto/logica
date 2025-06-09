9d5e3d8ac35b5f11d121e3392de99c07adac0774
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-06-09 11:21:11 -0300

    other remove way

--- teste.py
+++ teste.py
@@ -2,4 +2,10 @@ lista = ["casa", "comida", "cachorros", "carro", "paisagem", "computador", "tele
 
 lista.pop(1)
 
+#remove the last item
+#lista.pop()
+
+#remove the first item
+#lista.pop[0]
+
 print(lista)
