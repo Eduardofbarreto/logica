ab32c4b954e6473239a2fa6398f6fcb2bd3b6813
Eduardofbarreto <fbarreto.eduardo>
2025-06-17 16:53:36 -0300

    for-each

--- teste.cpp
+++ teste.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
+#include<string>
 
 int main(){
 
-    int numeros[10] = {1,2,3,4,5,6,7,8,9,10};
-    int num;
-    int resultado;
+    std::string nomes [5] = {"Eduardo", "Tânia", "Fernanda", "Mariana", "Murilo"};
+    std::string n;
 
-    std::cout<<"Digite um valor para num: "<<std::endl;
-    std::cin>>num;
-
-    for(int i = 0; i <10; i++){
-        resultado = num * numeros[i];
-        std::cout<<"O resultado de "<<num<<" x "<<numeros[i]<<" = "<<resultado<<std::endl;
+    for(std::string n : nomes){
+        std::cout<<n<<std::endl;
     }
+
 
 
     return 0;
