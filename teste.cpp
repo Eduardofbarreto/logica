52872cc98185e6ca7cdd40bc141bccdb6d8220a8
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-09-18 19:30:57 -0300

    calc

--- teste.cpp
+++ teste.cpp
@@ -2,9 +2,27 @@
 
 int main(){
 
-    for(int andar = 1; andar <= 10; andar++){
-        std::cout<<"O elevador está no "<<andar<<std::endl;
-    }
+    int num1, num2, soma, subt, mult;
+    float div;
 
-    return 0;
+    std::cout<<"Digite um valor para num1: "<<std::endl;
+    std::cin>>num1;
+
+    std::cout<<"Digite um valor para num2: "<<std::endl;
+    std::cin>>num2;
+
+    soma = num1 + num2;
+    subt = num1 - num2;
+    mult = num1 * num2;
+ 
+
+    std::cout<<"O resultado da soma é: "<<soma<<std::endl;
+    std::cout<<"O resultado da subtração é: "<<subt<<std::endl;
+    std::cout<<"O resultado da multiplicação é: "<<mult<<std::endl;
+    if(num2>0){
+        div = num1 / num2;
+        std::cout<<"O resultado da divisão é: "<<div<<std::endl;
+    }else{
+        std::cout<<"Erro: Divisão por 0!"<<std::endl;
+    }
 }
