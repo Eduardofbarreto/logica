749e286442511bee7e00a01ce4d8b12e0f1a8ad0
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-09-18 19:36:39 -0300

    temperatura em celsius

--- teste.cpp
+++ teste.cpp
@@ -1,28 +1,20 @@
+//(C×9/5)+32=F
+//(F−32)×5/9=C
+
 #include<iostream>
 
 int main(){
 
-    int num1, num2, soma, subt, mult;
-    float div;
+    float c, f;
+    float temp_c, temp_f;
 
-    std::cout<<"Digite um valor para num1: "<<std::endl;
-    std::cin>>num1;
+    std::cout<<"Digite a temperatura em Fahrenheit: "<<std::endl;
+    std::cin>>f;
 
-    std::cout<<"Digite um valor para num2: "<<std::endl;
-    std::cin>>num2;
+    temp_c = (f - 32)*5/9;
 
-    soma = num1 + num2;
-    subt = num1 - num2;
-    mult = num1 * num2;
- 
+    std::cout<<"A temperatura em Celsius é de: "<<temp_c<<std::endl;
 
-    std::cout<<"O resultado da soma é: "<<soma<<std::endl;
-    std::cout<<"O resultado da subtração é: "<<subt<<std::endl;
-    std::cout<<"O resultado da multiplicação é: "<<mult<<std::endl;
-    if(num2>0){
-        div = num1 / num2;
-        std::cout<<"O resultado da divisão é: "<<div<<std::endl;
-    }else{
-        std::cout<<"Erro: Divisão por 0!"<<std::endl;
-    }
+
+    return 0;
 }
