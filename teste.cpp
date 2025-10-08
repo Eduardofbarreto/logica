cec3d8814ae59578e5c3023839326730821cc409
Eduardofbarreto <fbarreto.eduardo@gmail.com>
2025-10-07 22:07:23 -0300

    soma com o digito final

--- teste.cpp
+++ teste.cpp
@@ -3,32 +3,17 @@
 
 int main(){
 
-    std::string user, password;
-    const std::string userCorreto = "edu";
-    const std::string passwordCorreto = "1234";
-    int numeroDeTentativas = 0;
-    const int maxTentativas = 3;
+    int n;
+    int soma = 0;
 
-    do{
-        std::cout<<"Digite o seu user: "<<std::endl;
-        std::cin>>user;
-        std::cout<<"Digite seu password: "<<std::endl;
-        std::cin>>password;
-        if(user == userCorreto && password == passwordCorreto){
-            std::cout<<"Login feito com sucesso!"<<std::endl;
-            break;
-        }else{
-            numeroDeTentativas++;
-            if(numeroDeTentativas < maxTentativas){
-                std::cout<<"Usuário ou senha incorretos! Tente novamente!"<<std::endl;
-                std::cout<<numeroDeTentativas<<std::endl;
-            }
-        }    
-    }while(numeroDeTentativas < maxTentativas);
-    
-    if(numeroDeTentativas == maxTentativas){
-        std::cout<<"Limite de tentativas atingido!"<<std::endl;
+    std::cout<<"Digite o número final da soma: "<<std::endl;
+    std::cin>>n;
+
+    for(int i = 1; i <= n; i++){
+        soma += i;
     }
+
+    std::cout<<soma<<std::endl;
 
     return 0;
 }
